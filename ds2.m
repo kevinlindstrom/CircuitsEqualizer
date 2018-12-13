3028c54c9dbc721aaa7e93c5b5edb2a45ac52f6c
kevinlindstrom <kevin.lindstrom.ct@gmail.com>
2018-12-13 18:02:05 -0500

    updated model

--- ds2.m
+++ ds2.m
@@ -1,18 +1,18 @@
 format short
 %Data extracted from oscilloscope
 filename = "F0022CH1.CSV";
-M = csvread(filename, 0, 3);
+%M = csvread(filename, 0, 3);
 %t = M(1:end, 1);
-V_s1 = M(1:end, 2);
-t = 0:.01:2;
+%V_s1 = M(1:end, 2);
+t = 0:.001:2;
 
 f_mid = 261.6255; %Hz
 f_low = 32.70; %Hz
 f_high = 2093.00; %Hz
 
-V_a = 1*cos(2*pi*f_low*t);
-V_b = 1*cos(2*pi*f_mid*t);
-V_c = 1*cos(2*pi*f_high*t);
+V_a = 1/2*cos(2*pi*f_low*t);
+V_b = 1/2*cos(2*pi*f_mid*t);
+V_c = 1/2*cos(2*pi*f_high*t);
 
 V_s = V_a + V_b + V_c;
 plot(V_s)
@@ -20,12 +20,12 @@ plot(V_s)
 C1 = 3.3e-07;
 R1 = 2000;
 R2 = 3300;
-C2 = 1*10^-6;
-R3 = 10;
-R4 = 10;
-C3 = 1*10^-6;
-R5 = 10;
-R6 = 10;
+C2 = 1*10^-7;
+R3 = 1100;
+R4 = 2200;
+C3 = 2.2*10^-8;
+R5 = 1000;
+R6 = 2000;
 
 % Voltages after band pass filters (Volts)
 Vas = V_s* -R2/(2*R1);
@@ -33,16 +33,16 @@ Vbs = V_s* -R4/(2*R3);
 Vcs = V_s* -R6/(2*R5);
 
 % Potentiometer proportions (out of one)
-Pa = 1;
-Pb = 1;
-Pc = 1;
+Pa = 0;
+Pb = 0;
+Pc = 0;
 
 % Amplifier resistances (Ohms)
-Rz = 5 *10^3;
-Rf = 5 *10^3;
-Ra = 2.2 *10^3;
-Rb = 2.2 *10^3;
-Rc = 2.2 *10^3;
+Rz = 1 *10^3;
+Rf = 2 *10^3;
+Ra = 5.1 *10^3;
+Rb = 5.1 *10^3;
+Rc = 5.1 *10^3;
 
 % Calcultated potentiometer resistances (do not alter)
 Rpa = Pa*10*10^3;
@@ -50,11 +50,12 @@ Rpb = Pb*10*10^3;
 Rpc = Pc*10*10^3;
 
 %Output Voltage
-V_out = Rpa*Rf/(Rz*Ra)*Vas + Rpb*Rf/(Rz*Rb)*Vbs + Rpc*Rf/(Rz*Rc)*Vcs;
+V_out = (Rpa+2000)*Rf/(Rz*Ra)*Vas + (Rpb+2000)*Rf/(Rz*Rb)*Vbs + (Rpc+2000)*Rf/(Rz*Rc)*Vcs;
 
 
 plot(t, V_out, 'g');
 hold on
+title('Voltage vs. Time')
 xlabel("Time")
 ylabel("Volts")
 
